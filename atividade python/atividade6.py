3e341cbcf8bf9b2d9c7aeb270d23e8beba6b0535
danielyoliveira-cloud <daniely_oliveira@estudante.sesisenai.org.br>
2026-03-04 20:16:51 +0000

    update

--- atividade python/atividade6.py
+++ atividade python/atividade6.py
@@ -3,10 +3,10 @@ idade = int (input ("digite a sua idade:"))
 
 #repete quando a idade for valida
 while idade > 120 or idade < 0:
-    idade = int(input("idade(anos completos - ate 120 anos):"))
-    dias_vida = idade*365
+    print = int(input("idade(anos completos - ate 120 anos):"))
+    dias_vida = idade * 365
     print (f"{nome},voce viveu {dias_vida}"
-else:
+else
     print("a idade digitada foi incorreta")
 
-#
+
